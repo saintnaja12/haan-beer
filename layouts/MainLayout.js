8b999a88942171f4078065480b195bafbfb3c9af
akaporn <brombom43531@gmail.com>
2022-12-10 00:00:46 +0700

    + nav bar

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -1,5 +1,26 @@
-import { Container } from "@nextui-org/react";
+import { Button, Container, Navbar, styled } from "@nextui-org/react";
+import { HiOutlineInformationCircle } from "react-icons/hi";
 
+const NavbarContainer = styled("div", {
+  width: "100%",
+  display: "flex",
+  justifyContent: "end",
+});
 export default function MainLayout({ children }) {
-  return <Container>{children}</Container>;
+  return (
+    <>
+      <Navbar maxWidth="fluid">
+        <NavbarContainer>
+          <Button
+            auto
+            ripple={false}
+            rounded
+            light
+            icon={<HiOutlineInformationCircle size={32} />}
+          />
+        </NavbarContainer>
+      </Navbar>
+      <Container>{children}</Container>
+    </>
+  );
 }
