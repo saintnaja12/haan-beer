3f121c0abe93ce6b1c785e2c06e97089c0691d0f
akaporn <brombom43531@gmail.com>
2023-01-09 10:33:56 +0700

    + vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,27 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import MainLayout from "../layouts/MainLayout";
 import { defaultTheme } from "../src/theme";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextThemesProvider
-      defaultTheme="dark"
-      attribute="class"
-      value={{
-        light: defaultTheme,
-        dark: defaultTheme,
-      }}
-    >
-      <NextUIProvider>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </NextUIProvider>
-    </NextThemesProvider>
+    <>
+      <NextThemesProvider
+        defaultTheme="dark"
+        attribute="class"
+        value={{
+          light: defaultTheme,
+          dark: defaultTheme,
+        }}
+      >
+        <NextUIProvider>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+        </NextUIProvider>
+      </NextThemesProvider>
+      <Analytics />
+    </>
   );
 }
 
